13b4c57bd94b7156a6539b7d986c80c5aa08e6e9
fortunium <51966164+fortunium@users.noreply.github.com>
2019-06-21 11:37:32 +0100

    Update tensorflow_test.py

--- tensorflow_test.py
+++ tensorflow_test.py
@@ -3,7 +3,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 x=2
 y=3
-op1=tf.add(x,y)
+z=4
+con11=tf.constant(2)
+con12=tf.constant(4)
+con13=tf.constant(5)
+op111=tf.multiply(x,con11)
+op112=tf.multiply(y,con12)
+op113=tf.multiply(z,con13)
+op11=tf.add(op111,op112,op113)
 with tf.Session() as sess:
-  op1=sess.run(op1)
-print(op1)
+  op11=sess.run(op11)
+print(op11)
